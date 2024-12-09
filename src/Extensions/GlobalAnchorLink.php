03005d6cef6bfafbf4d099951b80478aa6fcd0b8
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2024-12-09 14:38:04 +1300

    FIX: code cleanup

--- src/Extensions/GlobalAnchorLink.php
+++ src/Extensions/GlobalAnchorLink.php
@@ -28,7 +28,9 @@ class GlobalAnchorLink extends SuperLinkTypeExtension
 
     public function getLinkedGlobalAnchor(): ?string
     {
-        if (!$this->isLinkTypeMatch()) return null;
+        if (!$this->isLinkTypeMatch()) {
+            return null;
+        }
         $anchors = GlobalAnchors::get_anchors();
         $key = $this->getOwner()->getField('GlobalAnchorKey');
         return isset($anchors[$key]) ? $key : null;
@@ -36,21 +38,27 @@ class GlobalAnchorLink extends SuperLinkTypeExtension
 
     public function updateDefaultTitle(?string &$title): void
     {
-        if (!$this->isLinkTypeMatch()) return;
+        if (!$this->isLinkTypeMatch()) {
+            return;
+        }
         $anchor = $this->getOwner()->getLinkedGlobalAnchor();
         $title = GlobalAnchors::get_anchor_title($anchor);
     }
 
     public function updateURL(?string &$url): void
     {
-        if (!$this->isLinkTypeMatch()) return;
+        if (!$this->isLinkTypeMatch()) {
+            return;
+        }
         $anchor = $this->getOwner()->getLinkedGlobalAnchor();
         $url = empty($anchor) ? null : '#' . $anchor;
     }
 
     public function updateAbsoluteURL(?string &$url): void
     {
-        if (!$this->isLinkTypeMatch()) return;
+        if (!$this->isLinkTypeMatch()) {
+            return;
+        }
         $anchor = $this->getOwner()->getLinkedGlobalAnchor();
         if (empty($anchor)) {
             $url = null;
@@ -65,12 +73,14 @@ class GlobalAnchorLink extends SuperLinkTypeExtension
 
     public function updateCMSLinkTypeFields($fields, $type, $fieldPrefix): void
     {
-        if ($type !== 'globalanchor') return;
+        if ($type !== 'globalanchor') {
+            return;
+        }
         $fields->push(
             DropdownField::create(
                 $fieldPrefix . 'GlobalAnchorKey',
                 _t(__CLASS__ . '.GlobalAnchor', 'Global anchor'),
-                GlobalAnchors::get_anchors()
+                [] //GlobalAnchors::get_anchors()
             )
         );
     }
